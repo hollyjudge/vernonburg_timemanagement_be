f65e8f7652a08cf5f7b1dba40d25fd8d22a1dd40
Holly <100339429+hollyjudge@users.noreply.github.com>
2025-01-13 20:55:17 +0200

    Changed timesheet entry serialiser to include  project name and color.

--- time_management/serializers.py
+++ time_management/serializers.py
@@ -3,6 +3,20 @@ from rest_framework import serializers
 from .models import TimesheetEntry
 
 class TimesheetEntrySerializer(serializers.ModelSerializer):
+    project_name = serializers.CharField(source='project.name', read_only=True)
+    project_color = serializers.CharField(source='project.color', read_only=True)
+
     class Meta:
         model = TimesheetEntry
-        fields = '__all__'
+        fields = [
+            'id',
+            'user',
+            'project',
+            'project_name',  # Include project name
+            'project_color',  # Include project color
+            'work_date',
+            'hours_worked',
+            'task_number',
+            'notes',
+            'created_at',
+        ]
